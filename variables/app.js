6565e1569fb7b44ddc270fcdd45b99178ff308d9
ZubyDollarz22 <zubydollarz22@gmail.com>
2024-11-07 01:41:38 +0100

    Variables

---
+++ variables/app.js
@@ -0,0 +1,11 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(numOfSlices);
+console.log(favoriteFood);
+
+// let changeMe = true;
+let changeMe = false;
+console.log(changeMe);
+
+const entree = 'Enchiladas';
+console.log(entree);
