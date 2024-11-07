ead0bcc9eb889f31e900f36e365ba1fa3adfaebd
ZubyDollarz <zubydollarz22@gmail.com>
2024-11-07 02:50:58 +0100

    Dog years

---
+++ Dog years/main.js
@@ -0,0 +1,12 @@
+let myAge = 19;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(laterYears);
+console.log(earlyYears);
+
+let myAgeInDogYears = earlyYears + laterYears; // this adds the value of the two called variables together.
+
+myName = "ZubyDollarz".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
